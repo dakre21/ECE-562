8f98352b9462c7d23361fc4ada42dc3f0ac8589d
David Akre <dakre21@gmail.com>
2017-03-15 00:18:25 -0600

    CM'ing gem5, trying to get that building/running properly

---
+++ project/sim/setup_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+LD_LIBRARY_PATH=/usr/local/:/usr/local/lib:/usr/src/:/usr/src/linux-headers-3.16.0-30-generic/include/
+export LD_LIBRARY_PATH
---
+++ project/sim/setup_gem5.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Author: David Akre
+# Date: 3/14/2017
+# Description: Helper script to get local environment setup to run gem5
+
+echo "Setup is for ubuntu based systems"
+echo "If you're using RHEL change packet manager to yum"
+echo "Installation process takes approximately 10 mins"
+echo ""
+echo "Installing scons"
+sudo apt-get install scons
+echo ""
+echo "Installing swig"
+echo ""
+sudo apt-get install swig
+echo ""
+echo "Installing m4"
+echo ""
+sudo apt-get install m4
+echo ""
+echo "Installing zlib"
+echo ""
+cd ~
+git clone https://github.com/madler/zlib
+cd zlib
+./configure
+make
+sudo make install
+cd ~
+echo ""
+echo "Installing protobuf"
+echo ""
+git clone https://github.com/google/protobuf
+echo ""
+echo "Installing protobuf dependencies"
+echo ""
+sudo apt-get install autoconf automake libtool curl make g++ unzip
+echo ""
+cd protobuf
+./autogen.sh
+./configure
+make
+make check
+sudo make install
+sudo ldconfig 
+echo ""
+echo "Installing packages to improve gem5 performance"
+echo ""
+echo "Installing python-dev"
+echo ""
+sudo apt-get install python-dev
+echo "Installing tcmalloc"
+echo ""
+sudo apt-get install tcmalloc
+echo ""
+echo "Setting up environment"
+echo ""
+./setup_env.sh
+echo ""
+echo ""
+echo "Installing mercurial if you want to pull in updates from source"
+echo ""
+sudo apt-get install mercurial
+echo ""
